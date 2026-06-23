dd58f46c48aa8041468b3ac8409ba8a24c5d51de
darianaren <darianarengifo@gmail.com>
2026-06-23 15:28:36 -0300

    clean files

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { StyleSheet, Text, View } from "react-native";
+
+export default function HomeScreen() {
+  return (
+    <View>
+      <Text>Hola mundo</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
