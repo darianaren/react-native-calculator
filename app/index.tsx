2146131dc99633f8227bd058da977203017616be
darianaren <darianarengifo@gmail.com>
2026-06-29 09:30:17 -0300

    operations

--- app/index.tsx
+++ app/index.tsx
@@ -10,40 +10,55 @@ const isAndroid = Platform.OS === "android";
 if (isAndroid) NavigationBar.setBackgroundColorAsync("black");
 
 export default function HomeScreen() {
-  const { formula, buildNumber, clean, toggleSign, deleteLast } =
-    useCalculator();
+  const {
+    formula,
+    buildNumber,
+    clean,
+    toggleSign,
+    deleteLast,
+    prevNumber,
+    divideOperation,
+    multiplyOperation,
+    subtractOperation,
+    addOperation,
+    equalOperation,
+  } = useCalculator();
   return (
     <View style={styles.container}>
       <ThemeText>{formula}</ThemeText>
-      <ThemeText variant="h2">12</ThemeText>
+      {formula === prevNumber ? (
+        <ThemeText variant="h2"> </ThemeText>
+      ) : (
+        <ThemeText variant="h2">{prevNumber}</ThemeText>
+      )}
       <View style={styles.row}>
         <Button label="C" color="lightGray" onPress={clean} />
         <Button label="+/-" color="lightGray" onPress={toggleSign} />
         <Button label="del" color="lightGray" onPress={deleteLast} />
-        <Button label="÷" color="orange" onPress={() => console.log("")} />
+        <Button label="÷" color="orange" onPress={divideOperation} />
       </View>
       <View style={styles.row}>
         <Button label="7" onPress={() => buildNumber("7")} />
         <Button label="8" onPress={() => buildNumber("8")} />
         <Button label="9" onPress={() => buildNumber("9")} />
-        <Button label="x" color="orange" onPress={() => console.log("")} />
+        <Button label="x" color="orange" onPress={multiplyOperation} />
       </View>
       <View style={styles.row}>
         <Button label="4" onPress={() => buildNumber("4")} />
         <Button label="5" onPress={() => buildNumber("5")} />
         <Button label="6" onPress={() => buildNumber("6")} />
-        <Button label="-" color="orange" onPress={() => console.log("")} />
+        <Button label="-" color="orange" onPress={subtractOperation} />
       </View>
       <View style={styles.row}>
         <Button label="1" onPress={() => buildNumber("1")} />
         <Button label="2" onPress={() => buildNumber("2")} />
         <Button label="3" onPress={() => buildNumber("3")} />
-        <Button label="+" color="orange" onPress={() => console.log("")} />
+        <Button label="+" color="orange" onPress={addOperation} />
       </View>
       <View style={styles.row}>
         <Button label="0" doubleSize onPress={() => buildNumber("0")} />
         <Button label="." onPress={() => buildNumber(".")} />
-        <Button label="=" color="orange" onPress={() => console.log("")} />
+        <Button label="=" color="orange" onPress={equalOperation} />
       </View>
     </View>
   );
--- hooks/useCalculator.tsx
+++ hooks/useCalculator.tsx
@@ -8,11 +8,42 @@ enum Operator {
 }
 
 export const useCalculator = () => {
-  const lastOperation = useRef<Operator>();
+  const lastOperation = useRef<Operator | undefined>(undefined);
 
   const [formula, setFormula] = useState("");
   const [number, setNumber] = useState("0");
   const [prevNumber, setPrevNumber] = useState("0");
+
+  const calculateResult = () => {
+    const [firstValue, operation, secondValue] = formula.split(" ");
+
+    const num1 = Number(firstValue);
+    const num2 = Number(secondValue);
+
+    if (isNaN(num2)) return num1;
+
+    switch (operation) {
+      case Operator.add:
+        return num1 + num2;
+      case Operator.subtract:
+        return num1 - num2;
+      case Operator.multiply:
+        return num1 * num2;
+      case Operator.divide:
+        return num1 / num2;
+
+      default:
+        throw new Error(`Operation ${operation} not implemented`);
+    }
+  };
+
+  const equalOperation = () => {
+    const result = calculateResult();
+    setFormula(`${result}`);
+
+    lastOperation.current = undefined;
+    setPrevNumber("0");
+  };
 
   const buildNumber = (numberString: string) => {
     if (number.includes(".") && numberString === ".") return;
@@ -42,13 +73,54 @@ export const useCalculator = () => {
   };
 
   const deleteLast = () => {
-    if (number.length === 1) return setNumber("0");
+    if ((number.startsWith("-") && number.length === 2) || number.length === 1)
+      return setNumber("0");
     setNumber((prev) => prev.slice(0, -1));
   };
 
+  const setLastNumber = () => {
+    equalOperation();
+    if (number.endsWith(".")) {
+      setPrevNumber(number.slice(0, -1));
+    } else {
+      setPrevNumber(number);
+    }
+    setNumber("0");
+  };
+
+  const divideOperation = () => {
+    setLastNumber();
+    lastOperation.current = Operator.divide;
+  };
+
+  const multiplyOperation = () => {
+    setLastNumber();
+    lastOperation.current = Operator.multiply;
+  };
+
+  const subtractOperation = () => {
+    setLastNumber();
+    lastOperation.current = Operator.subtract;
+  };
+
+  const addOperation = () => {
+    setLastNumber();
+    lastOperation.current = Operator.add;
+  };
+
   useEffect(() => {
-    setFormula(number);
+    if (lastOperation.current) {
+      const firstFormulaPart = formula.split(" ").at(0);
+      setFormula(`${firstFormulaPart} ${lastOperation.current} ${number}`);
+    } else {
+      setFormula(number);
+    }
   }, [number]);
+
+  useEffect(() => {
+    const subResult = calculateResult();
+    setPrevNumber(`${subResult}`);
+  }, [formula]);
 
   return {
     // Props
@@ -61,5 +133,12 @@ export const useCalculator = () => {
     clean,
     toggleSign,
     deleteLast,
+
+    // Operations
+    divideOperation,
+    multiplyOperation,
+    subtractOperation,
+    addOperation,
+    equalOperation,
   };
 };
